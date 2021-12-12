c77cfd3d10eab19069c2de2378c8aa73cb16f36d
Niko <83379213+designatedcoder@users.noreply.github.com>
2021-12-12 12:40:11 -0500

    added project name to app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', "Coder's Shop") }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -18,9 +18,9 @@
     </head>
     <body class="font-sans antialiased">
         @inertia
-
+{{--
         @env ('local')
             <script src="http://localhost:3000/browser-sync/browser-sync-client.js"></script>
-        @endenv
+        @endenv --}}
     </body>
 </html>
