94015d8d9c521f38be8a977328d7e4cd8f9b8627
Niko <83379213+designatedcoder@users.noreply.github.com>
2021-12-12 12:39:23 -0500

    removed default route for the welcome page

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,6 @@ use Inertia\Inertia;
 |
 */
 
-// Route::get('/', function () {
-//     return Inertia::render('Welcome', [
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register'),
-//         'laravelVersion' => Application::VERSION,
-//         'phpVersion' => PHP_VERSION,
-//     ]);
-// });
-
 Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
