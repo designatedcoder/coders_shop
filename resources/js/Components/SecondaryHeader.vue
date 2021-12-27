9dfc1a5d99252028c68f60d5cd2882b160bb0973
Niko <83379213+designatedcoder@users.noreply.github.com>
2021-12-26 19:59:48 -0500

    Create SecondaryHeader.vue

---
+++ resources/js/Components/SecondaryHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <header class="text-gray-700 bg-gray-300 shadow">
+        <div class="flex justify-between items-center max-w-7xl mx-auto py-2 px-4 sm:px-6 lg:px-8">
+            <div class="flex items-center leading-tight">
+                <Link :href="route('welcome')" class="text-gray-700 transition hover:text-yellow-500">Home</Link>
+                <slot name="breadcrumbs"></slot>
+            </div>
+            <div class="w-1/2">
+                search
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+    import { defineComponent } from "vue";
+    import { Link } from '@inertiajs/inertia-vue3'
+    export default defineComponent({
+        components: {
+            Link,
+        }
+    })
+</script>
