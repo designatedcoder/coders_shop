cd5aad5aeb285f6aafb64099d875d851b105f896
Niko <83379213+designatedcoder@users.noreply.github.com>
2022-01-30 13:27:44 -0500

    Create YellowButton.vue

---
+++ resources/js/Components/Buttons/YellowButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <Link :href="href" :class="classes" v-if="as === 'href'">
+        <slot></slot>
+    </Link>
+    <button :class="classes" v-else>
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+    import { defineComponent } from "vue";
+    import { Link } from '@inertiajs/inertia-vue3'
+    export default defineComponent({
+        components: {
+            Link,
+        },
+        props: ['active', 'href', 'as'],
+        computed: {
+            classes() {
+                return 'inline-flex items-center px-4 py-2 bg-yellow-500 border border-transparent rounded font-semibold text-white uppercase tracking-widest hover:bg-gray-500 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 transition'
+            }
+        }
+    })
+</script>
