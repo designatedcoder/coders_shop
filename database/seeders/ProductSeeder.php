e3b58f7ca6d43a0b598b9f9d2241a6e89f0fe6a3
designatedcoder <83379213+designatedcoder@users.noreply.github.com>
2022-07-17 22:32:53 -0400

    Refactored Products seeder for images

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -22,7 +22,7 @@ class ProductSeeder extends Seeder
                 'slug' => 'women-'.$i,
                 'details' => 'women\'s hoodie',
                 'description' =>'Lorem '. $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
-                'main_image' => 'images/products/July2022/main_image/womens-'.$i.'.png',
+                'main_image' => 'images/products/main_image/womens-'.$i.'.png',
                 'product_code' => $category->category_code.'-00'.$i,
                 'price' => rand(999, 9999),
                 'quantity' => rand(1,10),
@@ -32,14 +32,14 @@ class ProductSeeder extends Seeder
         $product = Product::find(1);
         $product->update([
             'alt_images' => [
-                'images/products/July2022/alt_images/womens-1.png',
-                'images/products/July2022/alt_images/homegoods-1.png',
-                'images/products/July2022/alt_images/womens-2.png',
-                'images/products/July2022/alt_images/homegoods-2.png',
-                'images/products/July2022/alt_images/womens-3.png',
-                'images/products/July2022/alt_images/homegoods-3.png',
-                'images/products/July2022/alt_images/womens-4.png',
-                'images/products/July2022/alt_images/homegoods-4.png',
+                'images/products/alt_images/womens-1.png',
+                'images/products/alt_images/homegoods-1.png',
+                'images/products/alt_images/womens-2.png',
+                'images/products/alt_images/homegoods-2.png',
+                'images/products/alt_images/womens-3.png',
+                'images/products/alt_images/homegoods-3.png',
+                'images/products/alt_images/womens-4.png',
+                'images/products/alt_images/homegoods-4.png',
             ]
         ]);
         $product->categories()->attach(4);
@@ -52,7 +52,7 @@ class ProductSeeder extends Seeder
                 'slug' => 'mens-'.$i,
                 'details' => 'men\'s hoodie',
                 'description' =>'Lorem '. $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
-                'main_image' => 'images/products/July2022/main_image/mens-'.$i.'.png',
+                'main_image' => 'images/products/main_image/mens-'.$i.'.png',
                 'product_code' => $category->category_code.'-00'.$i,
                 'price' => rand(999, 9999),
                 'quantity' => rand(1,10),
@@ -67,7 +67,7 @@ class ProductSeeder extends Seeder
                 'slug' => 'kids-'.$i,
                 'details' => 'kid\'s hoodie',
                 'description' =>'Lorem '. $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
-                'main_image' => 'images/products/July2022/main_image/kids-'.$i.'.png',
+                'main_image' => 'images/products/main_image/kids-'.$i.'.png',
                 'product_code' => $category->category_code.'-00'.$i,
                 'price' => rand(999, 9999),
                 'quantity' => rand(1,10),
@@ -82,7 +82,7 @@ class ProductSeeder extends Seeder
                 'slug' => 'homegoods-'.$i,
                 'details' => 'homegoods',
                 'description' =>'Lorem '. $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
-                'main_image' => 'images/products/July2022/main_image/homegoods-'.$i.'.png',
+                'main_image' => 'images/products/main_image/homegoods-'.$i.'.png',
                 'product_code' => $category->category_code.'-00'.$i,
                 'price' => rand(999, 9999),
                 'quantity' => rand(1,10),
