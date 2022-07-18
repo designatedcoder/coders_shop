71bb489eb182459b8e0c20743ba7b9cd29052aa4
designatedcoder <83379213+designatedcoder@users.noreply.github.com>
2022-07-17 22:32:04 -0400

    Installed Livewire and Filament

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,6 +12,7 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/instantsearch.css@7.4.5/themes/satellite-min.css" integrity="sha256-TehzF/2QvNKhGQrrNpoOb2Ck4iGZ1J/DI4pkd2oUsBc=" crossorigin="anonymous">
+        @livewireStyles
 
         <!-- Scripts -->
         @routes
@@ -24,5 +25,6 @@
         @env ('local')
             <script src="http://localhost:3000/browser-sync/browser-sync-client.js"></script>
         @endenv --}}
+        @livewireScripts
     </body>
 </html>
